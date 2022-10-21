7b01eab9c45bf53028f8bd4a8a5892e7936f19a2
Guilherme Carvalho <guilherme.santos536@etec.sp.gov.br>
2022-10-21 18:35:18 -0300

    Aula JS 2010

---
+++ script.js
@@ -0,0 +1,31 @@
+let usuarios = []
+
+function Cadastrar() {
+   let cliente = {
+      Nome: "",
+      Idade: "",
+      CPF: "",
+      CEP: "",
+      Endereco: ""
+   }
+   cliente.Nome = document.getElementById("InputNome").value
+   cliente.Idade = document.getElementById("InputIdade").value
+   cliente.CPF = document.getElementById("InputCPF").value
+   cliente.CEP = document.getElementById("InputCEP").value
+   cliente.Endereco = document.getElementById("InputEndereco").value
+   
+   
+   usuarioLogado = document.getElementById("usuario")
+   usuarioLogado.innerText = cliente.Nome
+   usuarios.push(cliente)
+
+   //Exibir uma lista de usuários
+   var ul = document.getElementById("ListaUsuarios");
+   var li = document.createElement("li");
+   li.appendChild(document.createTextNode(cliente.Nome));
+   ul.appendChild(li);
+}
+
+function ExibirCadastros() {
+   console.log(usuarios)
+}
